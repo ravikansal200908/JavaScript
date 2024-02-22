3eb82aeb22867f39e93daa58bf5453823cf75515
Ravi Kansal <rkansal.pnp@gmail.com>
2024-02-22 20:43:17 +0530

    Questions

---
+++ PracticeQuestion/07.js
@@ -0,0 +1,10 @@
+function greetPerson(name) {
+    if (name = 'mohan') {
+        var greet = "Hello there."
+    } else {
+        var greet = "Hi there."
+    }
+    console.log("Greet is : " + greet)
+}
+
+greetPerson("mohan")
